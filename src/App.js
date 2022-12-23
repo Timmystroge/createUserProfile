98c2efa9530096064f7e2fa869d94dfbd70848e9
timmystroge <timmystroge75@gmail.com>
2022-12-24 00:43:03 +0100

    initial

--- src/App.js
+++ src/App.js
@@ -1,28 +1,9 @@
 import React from "react";
-import styled from "styled-components";
-import Lesson001 from "./lectures/Lesson001";
-import ReactComponents from "./lectures/ReactComponents";
-import ReactStylingPractice from "./lectures/ReactStylingPractice";
 
-const Layout = styled.div`
-padding: 0 20px;
-`
-const Header = styled.h3`
-  color: crimson;
-  padding: 3px;
-  font-family: cursive;
-  text-align: center;
-  text-decoration: underline;
-`;
 // ====>>>>><<<<<<======== //
 const App = () => {
   return (
-    <Layout>
-      <Header>Learning React Js With Angela Yu Favourite</Header>
-      {/* <Lesson001/> */}
-      {/* <ReactStylingPractice /> */}
-      <ReactComponents/>
-    </Layout>
+    <div>hello world </div>
   );
 };
 
