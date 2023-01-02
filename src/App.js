68bf3a1a74735ab8e386066c42cd3dd2c60d8246
timmystroge <timmystroge75@gmail.com>
2023-01-02 10:45:56 +0100

    feature

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import React, { useState } from "react";
 import CreateProfile from "./components/CreateProfile";
 import Profile from "./components/Profile";
-// import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-// import NotFound from "./components/404";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import NotFound from "./components/404";
 
 const App = () => {
+  // show profile
+  const [showProfile, setShowProfile] = useState(false);
+  function showProfiles() {
+    setShowProfile(true);
+  }
+  function hideProfiles() {
+    setShowProfile(false);
+  }
+  // show profile
+
   // profile state
   const [createNewProfile, setProfile] = useState([]);
 
@@ -16,30 +26,55 @@ const App = () => {
   }
   return (
     <>
-      <div>
-        <CreateProfile onAdd={addNewProfile} />
-        {createNewProfile.map((profileCreated, index) => {
-          <Profile
-            key={index}
-            id={index}
-            fname={profileCreated.firstName}
-            lname={profileCreated.LastName}
-            username={profileCreated.userName}
-            email={profileCreated.emailAddress}
-            address={profileCreated.address}
-            phone={profileCreated.phone}
-            gender={profileCreated.gender}
-          />;
-        })}
-      </div>
-
-      {/* <Router>
+      {/* ======= Router ====== */}
+      <Router>
         <Routes>
-          <Route exact path="/" element={<CreateProfile />}></Route>
-          <Route path="/profile" element={<Profile />}></Route>
+          <Route
+            exact
+            path="/"
+            element={<CreateProfile onAdd={addNewProfile} />}
+          ></Route>
           <Route path="*" element={<NotFound />}></Route>
         </Routes>
-      </Router> */}
+      </Router>
+      {/* ======= Router ====== */}
+
+      <div>
+        <div className="profilecard">
+          {createNewProfile != "" ? (
+            showProfile === false ? (
+              <button className="btn" onClick={showProfiles}>
+                View Profile Card
+              </button>
+            ) : (
+              <button className="btn" onClick={hideProfiles}>
+                Hide Profile Card
+              </button>
+            )
+          ) : null}
+          {/* display profils if show profile is clicked */}
+          {showProfile && (
+            <div className="wrapper">
+              {createNewProfile.map((profileCreated, index) => {
+                return (
+                  <Profile
+                    key={index}
+                    id={index}
+                    fname={profileCreated.firstName}
+                    lname={profileCreated.LastName}
+                    username={profileCreated.userName}
+                    email={profileCreated.emailAddress}
+                    address={profileCreated.address}
+                    phone={profileCreated.phone}
+                    gender={profileCreated.gender}
+                    // onDelete={}
+                  />
+                );
+              })}
+            </div>
+          )}
+        </div>
+      </div>
     </>
   );
 };
--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,46 +1,57 @@
+import { Delete } from "@material-ui/icons";
 import React from "react";
-// import { Link } from "react-router-dom";
 
-const Profile = ({id, fname, lname, username, email,address, phone, gender}) => {
+const Profile = ({
+  id,
+  fname,
+  lname,
+  username,
+  email,
+  address,
+  phone,
+  gender,
+  onDelete
+}) => {
+
+  function handleDelete() {
+    onDelete(id)
+  }
+
   return (
-    <div className="profilecard">
-      <h2>Profile Card</h2>
-      <h4 className="link">
-        {/* <Link to="/">Create Profile Card</Link> */}
-      </h4>
-      <div className="wrapper">
-        <section>
-          <div className="">
-            <h4>First Name</h4>
-            <p>{fname}</p>
-          </div>
-          <div className="">
-            <h4>Last Name</h4>
-            <p>{lname}</p>
-          </div>
-          <div className="">
-            <h4>UserName</h4>
-            <p>{username}</p>
-          </div>
-          <div className="">
-            <h4>Email</h4>
-            <p>{email}</p>
-          </div>
-          <div className="">
-            <h4>Address</h4>
-            <p>{address}</p>
-          </div>
-          <div className="">
-            <h4>Phone number</h4>
-            <p>{phone}</p>
-          </div>
-          <div className="">
-            <h4>Gender</h4>
-            <p>{gender}</p>
-          </div>
-        </section>
+    <section>
+    <div className="delete" onClick={handleDelete}>
+        <Delete />
       </div>
-    </div>
+      <div className="">
+        <h4>First Name</h4>
+        <p>{fname}</p>
+      </div>
+      <div className="">
+        <h4>Last Name</h4>
+        <p>{lname}</p>
+      </div>
+      <div className="">
+        <h4>UserName</h4>
+        <p>{username}</p>
+      </div>
+      <div className="">
+        <h4>Email</h4>
+        <p>{email}</p>
+      </div>
+      <div className="">
+        <h4>Address</h4>
+        <p>{address}</p>
+      </div>
+      <div className="">
+        <h4>Phone number</h4>
+        <p>{phone}</p>
+      </div>
+      <div className="">
+        <h4>Gender</h4>
+        <p>{gender}</p>
+      </div>
+      
+    </section>
   );
 };
 
